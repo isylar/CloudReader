4953c2fe41d71f570852c26da3516b5a02d9ced6
chenbing <chenbingbing@haizhi.com>
2017-01-16 16:19:31 +0800

    recorrect init

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="c2ee0a4a-8c8c-411a-862a-efbe75406c47", modulePackage="com.example.jingbin.cloudreader", sdkRoot="/Users/chenb/Library/Android/sdk",layoutInfoDir="/Users/chenb/AndroidStudioProjects/CloudReader/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=19,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
